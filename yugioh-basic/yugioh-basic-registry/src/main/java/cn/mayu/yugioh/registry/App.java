8d9c1340af3e286f565125b24e2c5a593c58ac3a
87194 <87194@DESKTOP-VCNCJL0>
2019-09-22 13:15:22 +0800

    first commit

---
+++ yugioh-basic/yugioh-basic-registry/src/main/java/cn/mayu/yugioh/registry/App.java
@@ -0,0 +1,16 @@
+package cn.mayu.yugioh.registry;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class App {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(App.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+}
