f34623e268018ff28dd56ee136b2f4ae81ab70ee
Viewshine123 <Viewshine123@DESKTOP-AJN8HPC>
2019-09-23 10:53:12 +0800

    spring cloud bus刷新配置

--- yugioh-reptile/yugioh-reptile-ourocg/src/main/java/cn/mayu/yugioh/reptile/ourocg/App.java
+++ yugioh-reptile/yugioh-reptile-ourocg/src/main/java/cn/mayu/yugioh/reptile/ourocg/App.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class App {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(App.class);
