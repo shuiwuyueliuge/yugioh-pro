9627c82dcc997533b47332cbfbc7428c593b16bd
Viewshine123 <Viewshine123@DESKTOP-AJN8HPC>
2019-09-23 17:10:49 +0800

    spring cloud sleuth

---
+++ yugioh-basic/yugioh-basic-track/src/main/java/cn/mayu/yugioh/basic/track/App.java
@@ -0,0 +1,16 @@
+package cn.mayu.yugioh.basic.track;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class App {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(App.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+}
