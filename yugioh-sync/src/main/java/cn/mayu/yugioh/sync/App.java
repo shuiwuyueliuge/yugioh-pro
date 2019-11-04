ba90a7614277e2fc0bba38c32b74e53596979e79
Viewshine123 <Viewshine123@DESKTOP-AJN8HPC>
2019-11-04 17:24:35 +0800

    sync

---
+++ yugioh-sync/src/main/java/cn/mayu/yugioh/sync/App.java
@@ -0,0 +1,13 @@
+package cn.mayu.yugioh.sync;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
