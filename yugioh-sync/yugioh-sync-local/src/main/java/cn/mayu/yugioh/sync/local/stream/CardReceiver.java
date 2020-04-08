f26fc1a034b886f1b251be32298dd97efcdcd57e
viewshine123 <lichao@viewshine.cn>
2020-04-08 17:14:51 +0800

    mq手动ack

--- yugioh-sync/yugioh-sync-local/src/main/java/cn/mayu/yugioh/sync/local/stream/CardReceiver.java
+++ yugioh-sync/yugioh-sync-local/src/main/java/cn/mayu/yugioh/sync/local/stream/CardReceiver.java
@@ -1,12 +1,12 @@
 package cn.mayu.yugioh.sync.local.stream;
 
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
-
 import com.rabbitmq.client.Channel;
-
 import cn.mayu.yugioh.common.dto.sync.home.CardProto;
 import cn.mayu.yugioh.common.dto.sync.home.CardProto.CardEntity;
 import cn.mayu.yugioh.sync.local.async.MqDataConsomer;
@@ -18,12 +18,18 @@ public class CardReceiver {
 	private MqDataConsomer dataConsomer;
 
 	@StreamListener(CardInputStream.CARD_SAVE_INPUT)
-	public void receiveSave(byte[] data
-			//,@Header(AmqpHeaders.CHANNEL) Channel channel,
-           // @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag
+	public void receiveSave(Message<byte[]> message
+			//byte[] data
+
             ) throws Exception {
+		//CardEntity entity = CardProto.CardEntity.parseFrom(data);
+		//dataConsomer.saveCard(entity);
+		
+		Channel channel = (com.rabbitmq.client.Channel)message.getHeaders().get(AmqpHeaders.CHANNEL);
+        Long deliveryTag = (Long) message.getHeaders().get(AmqpHeaders.DELIVERY_TAG);
+        channel.basicAck(deliveryTag, false);
+        byte[] data = message.getPayload();
 		CardEntity entity = CardProto.CardEntity.parseFrom(data);
-		// channel.basicAck(deliveryTag, false);//手动确认
 		dataConsomer.saveCard(entity);
 	}
 	
