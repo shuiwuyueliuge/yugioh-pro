949d961fd2ee63238cfc8e8b38ff8951281faab5
871942280@qq.com <871942280@qq.com>
2020-07-20 20:24:03 +0800

    'sync'

---
+++ yugioh-manager/src/main/java/cn/mayu/yugioh/manager/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package cn.mayu.yugioh.manager.config;
+
+import cn.mayu.yugioh.common.redis.RedisFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import java.util.List;
+
+@Configuration
+public class RedisConfig {
+
+    @Autowired
+    private ReactiveRedisConnectionFactory factory;
+
+    @Bean
+    public ReactiveRedisTemplate<Integer, List> initRedisTemplate() {
+        return RedisFactory.initIntegerRedisTemplate(factory, new Jackson2JsonRedisSerializer<>(List.class));
+    }
+}
--- yugioh-manager/src/main/java/cn/mayu/yugioh/manager/service/SyncServiceImpl.java
+++ yugioh-manager/src/main/java/cn/mayu/yugioh/manager/service/SyncServiceImpl.java
@@ -5,8 +5,12 @@ import cn.mayu.yugioh.common.dto.cardsource.SourceType;
 import cn.mayu.yugioh.common.facade.cardsource.CardSourceFacade;
 import cn.mayu.yugioh.common.facade.cardsource.PackageFacade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SyncServiceImpl implements SyncService {
@@ -17,9 +21,19 @@ public class SyncServiceImpl implements SyncService {
     @Autowired
     private CardSourceFacade cardSourceFacade;
 
+    @Autowired
+    private ReactiveRedisTemplate<Integer, List> packageListRedisTemplate;
+
     @Override
     public List<PackageData> gainPackageList(Integer sourceType) {
-        return packageFacade.gainPackageList(sourceType);
+        Optional<List> data = packageListRedisTemplate.opsForValue().get(sourceType).blockOptional();
+        if (data.isPresent()) {
+            return data.get();
+        }
+
+        List<PackageData> packageList = packageFacade.gainPackageList(sourceType);
+        packageListRedisTemplate.opsForValue().set(sourceType, packageList, Duration.ofDays(1L)).subscribe();
+        return packageList;
     }
 
     @Override
